96227651acc2204a1da030feffcc41b190bd29b3
Alexander Strasser <a.strasser@tutanota.de>
2022-05-24 23:04:38 +0200

    funktion declarations

---
+++ bubblesort_240522/randvect.h
@@ -0,0 +1,16 @@
+/*
+ * simple utility for creating arrays populated with random numbers.
+ * for testing and lerning with sorting algorithms
+ *
+ * Alexander Strasser
+ * 24.05.2022
+ */
+
+#ifdef RANDVECT_H
+#define RANDVECT_H
+
+int randvect_init();
+int* randvect(int length);
+
+
+#endif //RANDVECT_H
